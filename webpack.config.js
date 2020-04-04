f75307664fee5b15630f3b4ca36a5e639129ad60
Evgeny Shurupov <bolteg86@ya.ru>
2020-04-04 23:17:00 +0400

    Added typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
-const path = require('path');
+const path = require("path")
+
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
     },
-};
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}
