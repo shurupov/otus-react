12d2b8b205fabef28471fbb35cb46160707152f0
Evgeny Shurupov <bolteg86@ya.ru>
2020-04-27 00:47:41 +0400

    Lesson-7 Removed uncustomized story

--- lesson4/ConwayLife/ConwayLife.stories.tsx
+++ lesson4/ConwayLife/ConwayLife.stories.tsx
@@ -10,9 +10,4 @@ export default {
 
 const f = (x: number, y: number) => console.log(`(${x}, ${y})`);
 
-export const conway5x5 = () => <ConwayLife fieldWidth={5} fieldHeight={5} cellSize={10} onClick={f} cellAnimationDelay={50}/>;
-export const conway30x30 = () => <ConwayLife fieldWidth={30} fieldHeight={30} cellSize={10} onClick={f} cellAnimationDelay={50}/>;
-export const conway30x30WithBigCells = () => <ConwayLife fieldWidth={30} fieldHeight={30} cellSize={20} onClick={f} cellAnimationDelay={50}/>;
-export const conway60x60 = () => <ConwayLife fieldWidth={60} fieldHeight={60} cellSize={10} onClick={f} cellAnimationDelay={50}/>;
-export const conway120x120 = () => <ConwayLife fieldWidth={120} fieldHeight={120} cellSize={4} onClick={f} cellAnimationDelay={50}/>;
 export const customConway = () => <ConwayLife fieldWidth={number("field width", 30)} fieldHeight={number("field height", 30)} cellSize={number("cell size", 10)} onClick={f} cellAnimationDelay={50}/>;
