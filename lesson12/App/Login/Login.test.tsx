17b4b81111a34acb46e1fc4649534552adef4508
Evgeny Shurupov <bolteg86@ya.ru>
2020-05-28 00:52:23 +0400

    Lesson-12

--- lesson12/App/Login/Login.test.tsx
+++ lesson12/App/Login/Login.test.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import { Login } from "./Login";
-
-const mockHistory = { push: jest.fn() };
-jest.mock("react-router-dom", () => ({
-  useHistory: () => mockHistory,
-}));
 
 const authenticate = jest.fn();
 
 describe("LoginScreen", () => {
   it("navigates to user page on submit", async () => {
-    const name = "BobMarley";
     const screen = shallow(<Login onLogin={authenticate} />);
-
     expect(screen.find("Field").length).toEqual(1);
     expect(screen.find("button").length).toEqual(1);
   });
