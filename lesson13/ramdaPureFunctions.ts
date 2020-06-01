01742d81cc10cf2ad7fa259b9a157f175d35136d
Evgeny Shurupov <bolteg86@ya.ru>
2020-06-01 22:11:28 +0400

    lesson-13 Added using compose

--- lesson13/ramdaPureFunctions.ts
+++ lesson13/ramdaPureFunctions.ts
@@ -4,9 +4,11 @@ export type Team = { name: string; score: number };
 
 export const getTopName = (teams: Team[]): string => {
   let maxScoreTeam: Team = { name: "", score: 0 };
-  R.forEach(
-    (team: Team) =>
-      (maxScoreTeam = maxScoreTeam.score >= team.score ? maxScoreTeam : team)
+  R.compose(
+    R.forEach(
+      (team: Team) =>
+        (maxScoreTeam = maxScoreTeam.score >= team.score ? maxScoreTeam : team)
+    )
   )(teams);
   return maxScoreTeam.name;
 };
@@ -19,15 +21,13 @@ export const createQs = (qsObj: QsObj): string => {
   for (const key in qsObj) {
     params.push(`${key}=${qsObj[key]}`);
   }
-  return "?" + R.join("&")(params);
+  return "?" + R.compose(R.join("&"))(params);
 };
 
 // Задание 3
 
 export const parseQs = (qs: string): QsObj => {
   const result: QsObj = {};
-  R.forEach((s: string[]) => (result[s[0]] = s[1]))(
-    R.map((s: string) => s.split("="))(R.split("&")(qs.substr(1)))
-  );
+  R.compose(R.forEach((s: string[]) => (result[s[0]] = s[1])))(R.map((s: string) => s.split("="))(R.split("&")(qs.substr(1))));
   return result;
 };
