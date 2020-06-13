1049dff01498700ea3390c01300fc930fbd58566
Evgeny Shurupov <bolteg86@ya.ru>
2020-06-13 23:39:42 +0400

    Moved ConwayLife to App with Router

--- lesson4/index.tsx
+++ lesson4/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ConwayLifeApp } from "./App/ConwayLifeApp";
-import { ConwayLifeAppFormik } from "./App/ConwayLifeAppFormik";
+import { ConwayLifeApp } from "./ConwayLifeApp/ConwayLifeApp";
+import { ConwayLifeAppFormik } from "./ConwayLifeApp/ConwayLifeAppFormik";
 
 ReactDOM.render(<ConwayLifeApp />, document.getElementById("root"));
 // ReactDOM.render(<AppFormik />, document.getElementById("root"));
