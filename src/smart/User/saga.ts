c26208f467d38efaf1d167bf9bbe91c8372a320d
Evgeny Shurupov <bolteg86@ya.ru>
2020-07-10 02:28:03 +0400

    lesson-21 Added saga to working application

---
+++ src/smart/User/saga.ts
@@ -0,0 +1,48 @@
+import { AnyAction } from "redux";
+import { loginSlice } from "smart/User/slice";
+import { call, put, takeEvery } from "redux-saga/effects";
+
+export const sagaActionTypes = {
+  SAGA_LOGIN: "saga/user/login",
+  SAGA_LOGOUT: "saga/user/logout",
+};
+
+export const sagaLoginAction = (username: string) => {
+  return {
+    type: sagaActionTypes.SAGA_LOGIN,
+    payload: username,
+  };
+};
+
+async function fetchUser(username: string) {
+  await setTimeout(() => {}, 500);
+  return username;
+}
+
+export function* workerSagaLogin(action: AnyAction) {
+  const username = yield call(fetchUser, action.payload);
+  yield put(loginSlice.actions.login(username));
+}
+
+export function* watchSagaLogin() {
+  yield takeEvery(sagaActionTypes.SAGA_LOGIN, workerSagaLogin);
+}
+
+export const sagaLogoutAction = () => {
+  return {
+    type: sagaActionTypes.SAGA_LOGOUT,
+  };
+};
+
+async function clearSession() {
+  await setTimeout(() => {}, 500);
+}
+
+export function* workerSagaLogout() {
+  yield call(clearSession);
+  yield put(loginSlice.actions.logout());
+}
+
+export function* watchSagaLogout() {
+  yield takeEvery(sagaActionTypes.SAGA_LOGOUT, workerSagaLogout);
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,8 @@
-import { createStore, Store } from "redux";
+import { applyMiddleware, createStore, Store } from "redux";
 import { reducer } from "store/reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import { watchSagaLogin, watchSagaLogout } from "smart/User/saga";
 
 export interface ConwaySettings {
   fieldWidth: number;
@@ -17,4 +19,11 @@ export interface UserStore {
   username: string;
 }
 
-export const store: Store = createStore(reducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+
+export const store: Store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(watchSagaLogin);
+sagaMiddleware.run(watchSagaLogout);
