9715e5c249367d622e39fbbdfeebf3547aadea96
Evgeny Shurupov <bolteg86@ya.ru>
2020-07-23 03:28:39 +0400

    issue-19 fixed bug with session destroy after browser update

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,12 @@ import { applyMiddleware, createStore, Store } from "redux";
 import { reducer } from "store/reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
-import { watchSagaLogin, watchSagaLogout } from "smart/User/saga";
+import {
+  sagaRestoreSessionAction,
+  watchSagaLogin,
+  watchSagaLogout,
+  watchSagaRestoreSession,
+} from "smart/User/saga";
 import {
   reinitAction,
   watchSagaChangeSetting,
@@ -16,9 +21,11 @@ export const store: Store = createStore(
   reducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
+sagaMiddleware.run(watchSagaRestoreSession);
 sagaMiddleware.run(watchSagaLogin);
 sagaMiddleware.run(watchSagaLogout);
 sagaMiddleware.run(watchSagaUpdate);
 sagaMiddleware.run(watchSagaInit);
 sagaMiddleware.run(watchSagaChangeSetting);
+store.dispatch(sagaRestoreSessionAction());
 store.dispatch(reinitAction());
