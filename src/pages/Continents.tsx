4f90a49817e59ed786bc0072f53dcf4a04fbe280
Evgeny Shurupov <bolteg86@ya.ru>
2020-08-26 18:41:07 +0400

    lesson-29 Changed class component to function component

--- src/pages/Continents.tsx
+++ src/pages/Continents.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "smart/Continents/apollo";
 import { ContinentsList } from "smart/Continents/ContinentsList";
@@ -6,25 +6,23 @@ import { Route } from "react-router";
 import { CountriesList } from "smart/Continents/CountriesList";
 import { Country } from "smart/Continents/Country";
 
-export class Continents extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Continents and Countries</h1>
-        <ApolloProvider client={client}>
-          <Route exact path="/continents" component={ContinentsList} />
-          <Route
-            exact
-            path="/continents/:continentCode"
-            component={CountriesList}
-          />
-          <Route
-            exact
-            path="/continents/:continentCode/:countryCode"
-            component={Country}
-          />
-        </ApolloProvider>
-      </div>
-    );
-  }
-}
+export const Continents = () => {
+  return (
+    <div>
+      <h1>Continents and Countries</h1>
+      <ApolloProvider client={client}>
+        <Route exact path="/continents" component={ContinentsList} />
+        <Route
+          exact
+          path="/continents/:continentCode"
+          component={CountriesList}
+        />
+        <Route
+          exact
+          path="/continents/:continentCode/:countryCode"
+          component={Country}
+        />
+      </ApolloProvider>
+    </div>
+  );
+};
